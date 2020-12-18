92ff3e2ce0862d92da914d0c5d7ee9df755c3263
Bing Z <bingz@me.com>
2020-12-18 01:38:52 -0700

    setup server n mongodb atlas connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 const app = express();
 const path = require('path');
 
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 //use express middlewaree
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 //For heroku deployment - this block of codes will only run in production env
 if (process.env.NODE_ENV === 'production') {
