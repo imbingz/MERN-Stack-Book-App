75f6dab6df36c741888cf441feb313722bd65f66
Bing Z <bingz@me.com>
2020-12-29 17:18:06 -0700

    setup initial search form

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Search() {
-	return <div>Serach page</div>;
+	const queryRef = useRef();
+
+	const handleFetchBook = async e => {
+		e.preventDefault();
+		let query = queryRef.current.value;
+
+		const response = await fetch(``);
+
+		queryRef.current.value = '';
+	};
+
+	return (
+		<main>
+			<h1>Discover Your Next Read </h1>
+			<form onSubmit={handleFetchBook}>
+				<label htmlFor='query' />
+				<input id='query' name='query' ref={queryRef} placeholder='Enter a Book or Autthor name ... ' type='text' />
+				<button type='submit'>Search</button>
+			</form>
+		</main>
+	);
 }
 
 export default Search;
