0756ebe8d08a92cc5c20d531916cbbc5454fc822
Bing Z <bingz@me.com>
2021-01-01 02:45:56 -0700

    added loading img

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Spinner, Button } from 'react-bootstrap';
 
 const Loading = () => {
 	return (
-		<div className='loading'>
-			<h1>loading...</h1>
-		</div>
+		<Button variant='primary' disabled className='m-5 py-3 px-5'>
+			<Spinner as='span' animation='grow' size='sm' role='status' aria-hidden='true' />
+			<h3>Loading...</h3>
+		</Button>
 	);
 };
 
