71468e7d5e90fa3c7763fc5f6116edfd1e92025f
Bing Z <bingz@me.com>
2021-01-01 15:09:59 -0700

    added toastify and footer in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,25 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Save from './pages/Save';
 import Search from './pages/Search';
 import Navbar from './components/Navbar';
-import GlobalProvider from './utils/GlobalContext'
+import Footer from './components/Footer';
+import GlobalProvider from './utils/GlobalContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+function App () {
 	return (
 		<BrowserRouter>
 			<GlobalProvider>
-			<Navbar />
-			<div className='App'>
-				<Switch>
-					<Route exact path='/' component={Search} />
-					<Route exact path='/save' component={Save} />
-				</Switch>
-			</div>
+				<Navbar />
+				<div className='App'>
+					<Switch>
+						<Route exact path='/' component={Search} />
+						<Route exact path='/save' component={Save} />
+					</Switch>
+				</div>
+				<ToastContainer transition={Zoom} autoClose={3000} />
+				<Footer />
 			</GlobalProvider>
 		</BrowserRouter>
 	);
