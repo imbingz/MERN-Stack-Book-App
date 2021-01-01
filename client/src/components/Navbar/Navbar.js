f637698c57a477e2e854cffaa108fa1dde10dac4
Bing Z <bingz@me.com>
2021-01-01 02:43:06 -0700

    added conditional rendering

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 
 function Navbar() {
+    const [isSearch, setIsSearch] = useState(false)
+    const location = useLocation();
+
+    console.log(location.pathname);
+    
+    useEffect(() => {
+        if(location.pathname === '/save') {
+            setIsSearch(true)
+        }  
+        if(location.pathname === '/')  {
+            setIsSearch(false)
+        }   
+    }, [location.pathname])
 
 	return (
 		<nav className='Navbar'>
 			<div className='Navbar-brand'>
-				<span>Book Search</span>
+				<Link to='/'><img src='assets/book-logo.png' style={{width: 32, height: 32}} alt='book-logo'/> </Link>
 			</div>
             
 			<ul className='Navbar-links'>
-                <li className='Navbar-link'>
-					<Link to='/'> Search </Link>
-				</li> 
-                <li className='Navbar-link'>
+                {isSearch && <li className='Navbar-link'>
+					<Link to='/'> Search Books </Link>
+				</li>}
+                {!isSearch && <li className='Navbar-link'>
 					<Link to='/save'> View Saved Books </Link>
-				</li>		
+				</li>}		
+                	
 			</ul>
 		</nav>
 	);
