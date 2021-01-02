23e275980cc0463833670b3503a42c3c41a4aa10
Bing Z <bingz@me.com>
2021-01-01 19:51:42 -0700

    added saved icon n link

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 import { AiFillHome } from 'react-icons/ai';
+import { BsBookmarks } from 'react-icons/bs';
 
 function Footer () {
 	return (
 		<footer>
 			<Navbar bg='dark' fixed='bottom' className='Footer justify-content-around'>
 				<div>
-					<Link to='/'>
-						<AiFillHome size={28} style={{ fill: 'white' }} />{' '}
+					<Link to='/' className='pl-2'>
+						<AiFillHome size={22} style={{ fill: '#1c0' }} />{' '}
 					</Link>{' '}
+                   <Link to='/save' className='px-2'>< BsBookmarks size={ 20 } style={ { fill: 'yellow' } } /></Link>
 					<small className='text-muted mx-5 px-5'>
 						{' '}
 						Copyright &copy; 2020 by Bing Z. <span style={{ color: 'red' }}>&hearts;</span>{' '}
