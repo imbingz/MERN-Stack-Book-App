75484b34d30a1059a99cad9b17038caaae2d440b
Bing Z <bingz@me.com>
2021-01-02 21:38:03 -0700

    changed loading spinners n added styles

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -3,10 +3,13 @@ import { Spinner, Button } from 'react-bootstrap';
 
 const Loading = () => {
 	return (
-		<Button variant='primary' disabled className='m-5 py-3 px-5'>
-			<Spinner as='span' animation='grow' size='sm' role='status' aria-hidden='true' />
-			<h3>Loading...</h3>
-		</Button>
+        <div className='m-5 p-5'>
+            <h1>Loading...</h1>
+            <Spinner animation="border" variant="primary" className='mx-5'/>
+            <Spinner animation="border" variant="success" />
+            <Spinner animation="border" variant="danger" className='mx-5'/>
+            <Spinner animation="border" variant="warning" />
+        </div>
 	);
 };
 
