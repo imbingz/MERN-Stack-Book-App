be3a023299457e94d8497fb7839fbfe3a641d977
Bing Z <bingz@me.com>
2021-01-02 21:37:32 -0700

    added home page link in error component

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Error() {
-	return <h1>404... Not Found</h1>;
+    return (
+        <div className='m-5 p-5'>
+            <h1>404... Not Found</h1>;
+            <h2><Link to='/'>Click HERE</Link> to Search Books</h2>
+       </div>
+    )
+   
 }
 
 export default Error;
